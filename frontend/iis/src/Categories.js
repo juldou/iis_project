3166109d3e8f0c3df4080d25ee16b79add447b8c
rocmartin <xrocka00@stud.fit.vutbr.cz>
2019-11-26 19:00:37 +0100

    basic fronetnd

--- frontend/iis/src/Categories.js
+++ frontend/iis/src/Categories.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './index.css';
 import Configuration from "./Network/Configuration";
 import NetworkService from "./Network/NetworkService";
+import './Categories.css'
 
 class Categories extends Component {
 
@@ -37,7 +38,7 @@ class Categories extends Component {
         );
         return (
             <div className="Categories">
-                <ul className="items">
+                <ul className="filter-items">
                     {listItems}
                 </ul>
 
