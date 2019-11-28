b7a73d85eeaf93341903394d39f57caa4fa66f51
Julius Marko <julo.marko@gmail.com>
2019-11-29 00:45:52 +0100

    return foods with orders

--- backend/api_playground/get_orders.sh
+++ backend/api_playground/get_orders.sh
@@ -1,5 +1,5 @@
 curl -i -H "Accept: application/json" \
--H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNvdXJpZXJAY291cmllci5jb20iLCJyb2xlIjoiY291cmllciIsImV4cCI6MTU3NDg4NDkwN30.5SkTY_2coC3epOhPzGWkR2i5wrKM_1chcqi4Z7qdPRs" \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImV4cCI6MTU3NTAxMjc5OX0.OMB7wkCS9G7ewxhsMTiVi2YQ9Yt8zRDVh3vUXah3yjI" \
 -X GET http://localhost:9092/api/orders/courier
 
 
