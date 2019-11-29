b64668b8edba244b1bd2f81a7c54fde541d6fdb9
jan <zaus.jan@gmail.com>
2019-11-30 00:05:25 +0100

    button fix

--- frontend/iis/src/Order/Cart.js
+++ frontend/iis/src/Order/Cart.js
@@ -86,6 +86,14 @@ class Cart extends Component{
             return <Redirect to='/orders' />
         }
 
+        if(this.state.order.length === 0) {
+            return (
+                <div className="center">
+                    <h3>Cart is empty</h3>
+                </div>
+                )
+        }
+
             let addedItems = this.state.order.map(item=>{
                     return(
 
